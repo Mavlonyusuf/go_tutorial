fcd93205f034a7ed4f4d3e98e719d64d08ed5750
Mavlonyusuf <mavlonjonyusupov1@gmail.com>
2025-07-11 15:58:43 +0500

    I have finished all data types

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
-import "fmt"
-
+import (
+	"fmt"
+)
 func main() {
-    fmt.Println("Salom Dunyo")
+    var fruits = [4] string {"Apple", "Peach", "Pineapple", "cherry"}
+    narxlar := [3] float32 {25.990, 32.900, 12.78}
+    yangi_narxlar := [5] float32 {1: 19.990, 4: 12.980}
+    fmt.Println(fruits)
+    fmt.Println(narxlar)
+    fmt.Println(yangi_narxlar)
 }
 
 
